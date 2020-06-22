7993713ce7a92b733e7000d720215acf75c96d07
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-22 16:44:39 -0400

    playgrounded

---
+++ playground.js
@@ -0,0 +1,16 @@
+var num1 = 2
+var num2 = 3
+
+function addNumbers() {
+                let results = (num1 + num2) * 3;
+                console.log(results)
+
+                if (results > 50) {
+                console.log('Result is bigger');
+            }
+            else {
+                console.log('Result in smaller')
+            }
+            };
+  addNumbers();
+
